3fcb45bb34565ed498aa489fd38415bf17e41b97
Alex Cardelle <acardelle@icloud.com>
2019-02-06 22:24:34 -0500

    Reformulated program to accurately display "Hello World" as a simple Spring Boot project

---
+++ spendermap-program/src/test/java/com/cardelle/spendermapprogram/SpendermapProgramApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cardelle.spendermapprogram;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpendermapProgramApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
